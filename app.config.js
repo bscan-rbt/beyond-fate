3aaf501e728cbe357dbef3ff4273c02550962ebe
Ben <bendscanlon@gmail.com>
2025-07-11 09:38:39 -0400

    Updated Vite config

--- app.config.js
+++ app.config.js
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  server: {
+    allowedHosts: 'devserver-main--beyond-fate.netlify.app'
   }
 });
