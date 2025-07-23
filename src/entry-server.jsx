5e25b8039fac8a53c5895d3fba02fa0ed31d5dcb
Ben <bendscanlon@gmail.com>
2025-07-23 16:06:40 -0400

    --

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -9,6 +9,7 @@ export default createHandler(() => (
           <title>Beyond Fate</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="A fantasy deckbuilding game built around traditional TTRPG mechanics."/>
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
